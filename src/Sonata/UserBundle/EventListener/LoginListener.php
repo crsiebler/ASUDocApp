11a6a1b2748b52189aff72abf0cd57a89e6f4427
Cory Siebler <csiebler@asu.edu>
2013-04-11 11:03:00 -0700

    Fixed LoginListener

--- src/Sonata/UserBundle/EventListener/LoginListener.php
+++ src/Sonata/UserBundle/EventListener/LoginListener.php
@@ -2,25 +2,26 @@
 
 namespace Sonata\UserBundle\EventListener;
 
-use Symfony\Component\Security\Http\Authentication\AuthenticationSuccessHandlerInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\SecurityContext;
+use Symfony\Component\Security\Http\Authentication\AuthenticationSuccessHandlerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\RedirectResponse;
-use Symfony\Component\Routing\Router;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+
 
 class LoginListener implements AuthenticationSuccessHandlerInterface {
 
     protected $router;
     protected $security;
 
-    public function __construct(Router $router, SecurityContext $security) {
+    public function __construct(UrlGeneratorInterface $router, SecurityContext $security) {
         $this->router = $router;
         $this->security = $security;
     }
 
     public function onAuthenticationSuccess(Request $request, TokenInterface $token) {
-        if ($this->security->isGranted('ROLE_SUPER_ADMIN')) {
+        if ($this->security->isGranted('ROLE_SITE-ADMIN')) {
             $response = new RedirectResponse($this->router->generate('category_index'));
         } elseif ($this->security->isGranted('ROLE_ADMIN')) {
             $response = new RedirectResponse($this->router->generate('category_index'));
